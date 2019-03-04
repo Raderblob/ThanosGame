2b4ffe0455e29a8be7ffd9aedb860403c61fae8b
Raderblob <39084971+Raderblob@users.noreply.github.com>
2019-03-04 13:40:07 +0100

    Reintegration

--- Main.java
+++ Main.java
@@ -2,7 +2,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        Game.launchGame();
+
+            MenuPrincipal mn = new MenuPrincipal();
+      //  Game.launchGame();
 
     }
 }
