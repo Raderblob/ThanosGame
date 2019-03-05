b39d95c9feb65bdf29ee9d3f337caa0aa9d54392
Raderblob <39084971+Raderblob@users.noreply.github.com>
2019-03-05 18:15:16 +0100

    Add terrain base noise

--- World.java
+++ World.java
@@ -7,15 +7,17 @@ public class World {
     PlayerClass Thanos;
     byte [][] terrain;
     SimplexNoise geny;
+    SimplexNoise genlim;
     double counter =0;
 
 
     public World(int worldType,PlayerClass p){
         Thanos = p;
-        geny= new SimplexNoise(   (int)System.currentTimeMillis(),8,0.1,0.5);
+
         switch (worldType){
             case 0 :
-
+                geny= new SimplexNoise(   (int)System.currentTimeMillis(),8,0.1,0.5);
+                genlim = new SimplexNoise(   (int)System.currentTimeMillis()+1,8,0.1,0.5);
             break;
             default:
 
@@ -31,11 +33,12 @@ public class World {
         gc.clearRect(0,0,6000,6000);
         for(int x = 0 ;x <500;x++){
             for(int y = 0;y<500;y++){
-                    double a =Math.abs(geny.getNoise(x*0.05,y*0.05))*(y/500d);
-                    gc.setFill(new Color(a,a,a,1));
-                   gc.fillRect(x,y,1,1);
+                if((y/500d)>Math.abs(genlim.getNoise(x*0.015,0))) {
+                    double a = Math.abs(geny.getNoise(x * 0.05, y * 0.05)) ;
+                    gc.setFill(new Color(a, a, a, 1));
+                    gc.fillRect(x, y, 1, 1);
 
-
+                }
             }
         }
     }
