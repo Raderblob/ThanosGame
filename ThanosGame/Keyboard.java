89dc924e4e72186fefe70bdab13a75c319bb172a
Nireda-52 <adrien52@hotmail.com>
2019-03-11 09:52:53 +0100

    Keyboard

--- ThanosGame/Keyboard.java
+++ ThanosGame/Keyboard.java
@@ -3,6 +3,32 @@ package ThanosGame;
 import javafx.scene.input.KeyCode;
 
 public class Keyboard {
-    public KeyCode right = KeyCode.D;
-
+    public static KeyCode right;
+    public static KeyCode left;
+    public static KeyCode jump;
+    public static boolean inQWERTY;
+    public Keyboard(){
+        right = KeyCode.D;
+        left = KeyCode.A;
+        jump = KeyCode.Q;
+        inQWERTY = true;
+    }
+    public static void setAZERTY(){
+        right = KeyCode.D;
+        left = KeyCode.Q;
+    }
+    public static void setQWERTY(){
+        right = KeyCode.D;
+        left = KeyCode.A;
+    }
+    public static void setSpaceJump(){
+        jump = KeyCode.SPACE;
+    }
+    public static void setUpJump(){
+        if(inQWERTY){
+            jump = KeyCode.Q;
+        }else{
+            jump = KeyCode.Z;
+        }
+    }
 }
