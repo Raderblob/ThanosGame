670af8c01e4d4d53d4c6e9b2f3c44b233785572d
Nireda-52 <adrien52@hotmail.com>
2019-03-16 15:53:34 +0100

    Mise à jour du Clavier (Keyboard) Mise à jour de Option (rajout du menu déroulant de saut et correction du listener AZERT/QWERTY) Mise à jour de l'ouverture d'Option (correction du bug qui recréait un menu option à chaque fois)

--- ThanosGame/Keyboard.java
+++ ThanosGame/Keyboard.java
@@ -3,30 +3,29 @@ package ThanosGame;
 import javafx.scene.input.KeyCode;
 
 public class Keyboard {
-    public static KeyCode right;
-    public static KeyCode left;
-    public static KeyCode jump;
-    public static boolean inQWERTY;
+    public static KeyCode right, left, jump, down;
+    public static boolean isInQWERTY;
     public Keyboard(){
         right = KeyCode.D;
         left = KeyCode.A;
-        jump = KeyCode.Q;
-        inQWERTY = true;
+        jump = KeyCode.W;
+        down = KeyCode.S;
+        isInQWERTY = true;
     }
     public static void setAZERTY(){
-        right = KeyCode.D;
         left = KeyCode.Q;
+        isInQWERTY = false;
     }
     public static void setQWERTY(){
-        right = KeyCode.D;
         left = KeyCode.A;
+        isInQWERTY = true;
     }
     public static void setSpaceJump(){
         jump = KeyCode.SPACE;
     }
     public static void setUpJump(){
-        if(inQWERTY){
-            jump = KeyCode.Q;
+        if(isInQWERTY){
+            jump = KeyCode.W;
         }else{
             jump = KeyCode.Z;
         }
