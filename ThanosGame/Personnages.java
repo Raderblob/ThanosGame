13b619a7fdac682ce366f58ccc2642c8d2d0948e
LouiseLG <48202489+LouiseLG@users.noreply.github.com>
2019-03-21 09:18:49 +0100

    Début de la classe personnages

---
+++ ThanosGame/Personnages.java
@@ -0,0 +1,42 @@
+/*package ThanosGame;
+
+public class Personnages {
+    private int largeur, taille; // Taille perso
+    private int x, y; // position
+    private boolean marche;
+    private boolean aDroite;
+    public int compt; // nb de pas du personnage
+
+    // CONSTRUCTEUR
+    public Personnages(int x, int y, int largeur, int taille) {
+
+    this.x=x; this.y=y;
+    this.largeur=largeur;this.taille=taille;
+    this.marche=false; this.aDroite=true;
+    this.compt=0;
+}
+    //GETTERS
+
+    public int getLargeur() {return largeur; }
+    public int getTaille() {return taille; }
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public boolean isMarche() { return marche; }
+    public boolean isaDroite() { return aDroite; }
+
+    //SETTERS
+
+    public void setLargeur(int largeur) {this.largeur = largeur;}
+    public void setTaille(int taille) { this.taille = taille; }
+    public void setX(int x) { this.x = x; }
+    public void setY(int y) { this.y = y; }
+    public void setMarche(boolean marche) { this.marche = marche; }
+    public void setaDroite(boolean aDroite) { this.aDroite = aDroite; }
+    public void setCompt(int compt) { this.compt = compt; }
+
+    // METHODES UTILISABLES
+    // Deplacement du personnage
+
+
+}
+*/
