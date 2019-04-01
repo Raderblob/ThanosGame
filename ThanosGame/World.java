140c6e11b62f62001f1fbf749695454b997dcf8f
LouiseLG <48202489+LouiseLG@users.noreply.github.com>
2019-04-01 13:40:56 +0200

    Affichage des ennemis + mouvement (recherche de l'erreur)

--- ThanosGame/World.java
+++ ThanosGame/World.java
@@ -44,7 +44,7 @@ public class World {
                 terrain = new TerrainMap(2,false,this,enemies);
                 System.out.println("loading base");
                 new LargeBase(BuildingSaves.thanosBase,new Point2D(0,0)).changeTerrain(terrain);//generate home base
-                teleporters.add(new Teleporter(new Point2D(1000,320),1,myGame));
+                teleporters.add(new Teleporter(new Point2D(600,100),1,myGame));
                 break;
             default:
                 starterPos = new Point2D(50,50);
