15aa6669d570fc3b1aefa2633405414ec27f3545
Raderblob <39084971+Raderblob@users.noreply.github.com>
2019-04-02 15:24:38 +0200

    tnt space stone

--- ThanosGame/graphics/images/ImagesSaves.java
+++ ThanosGame/graphics/images/ImagesSaves.java
@@ -7,7 +7,7 @@ public class ImagesSaves {
     public static Image thanosSprites;
     public static Image teleporterSprite;
     public static Image shieldSprite;
-    public static Image projectiles[] = new Image[3];
+    public static Image projectiles[] = new Image[4];
     public static Image explosions[] = new Image[1];
     public static Image shieldSprites;
     public static Image wakandaisSprites;
@@ -17,6 +17,7 @@ public class ImagesSaves {
         projectiles[0] =  new Image(ImagesSaves.class.getResource("projectile1.png").toExternalForm());
         projectiles[1] =  new Image(ImagesSaves.class.getResource("projectile2.png").toExternalForm());
         projectiles[2] =  new Image(ImagesSaves.class.getResource("projectile3.png").toExternalForm());
+        projectiles[3] =  new Image(ImagesSaves.class.getResource("projectile4.png").toExternalForm());
         explosions[0]=  new Image(ImagesSaves.class.getResource("explosion1.png").toExternalForm());
         teleporterSprite =  new Image(ImagesSaves.class.getResource("teleporterSprite.png").toExternalForm());
         shieldSprites = new Image(ImagesSaves.class.getResource("ShieldSprites.png").toExternalForm());
