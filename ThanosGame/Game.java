3406d6d488448088f40f28a7b21bc3835d85c799
Raderblob <39084971+Raderblob@users.noreply.github.com>
2019-04-11 16:33:02 +0200

    new uml 2

--- ThanosGame/Game.java
+++ ThanosGame/Game.java
@@ -139,7 +139,7 @@ public class Game extends Application {
         Scale tScale = new Scale(windowScale, windowScale);//autoscale to screen size
         root.getTransforms().add(tScale);
         scene = new Scene(root, winParam.getX() * windowScale, winParam.getY() * windowScale);
-        stage.setTitle("ThanosGame.Thanos rules the world v2.2");
+        stage.setTitle("ThanosGame.Thanos rules the world v2.4");
         stage.setScene(scene);
 
         Canvas canvas = new Canvas(winParam.getX(), winParam.getY());
